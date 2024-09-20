b46879b49c7843dcfdac4fe0966881d8b0a5352b
Your Name <you@example.com>
2024-09-20 10:27:49 +0200

    footer

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navbarLinks = document.querySelector('.navbar-links');
+  
+    hamburger.addEventListener('click', function() {
+      // Toggle the 'active' class on the navbar links
+      navbarLinks.classList.toggle('active');
+    });
+  });
